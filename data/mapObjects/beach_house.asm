d31b57a89d49bf63f64e1723cd926e196070e24c
SteppoBlazer <gainzgamezn@gmail.com>
2019-09-07 17:16:55 +0200

    Add warp, sign, and warp_to macros for data/mapObjects definitions, in preparation for compatibility with Polished Maps

--- data/mapObjects/beach_house.asm
+++ data/mapObjects/beach_house.asm
@@ -2,8 +2,8 @@ BeachHouseObjects: ; 0xf23a4
 	db $a ; border block
 
 	db 2 ; warps
-	db 7,2,0,$ff
-	db 7,3,0,$ff
+	warp 2, 7, 3, $ff
+	warp 3, 7, 7, $ff
 
 	db 4 ; signs
 	db 0,3,3
@@ -12,9 +12,9 @@ BeachHouseObjects: ; 0xf23a4
 	db 1,$d,6
 
 	db 2 ; objects
-	object SPRITE_FISHER, 2, 3, STAY, DOWN, 1 ; surfin' dude
-	object $3d, 5, 3, WALK, $01, 2 ; pikachu
+	object SPRITE_FISHER, 2, 4, STAY, RIGHT, 1 ; person
+	object SPRITE_PIKACHU, 2, 3, WALK, RIGHT, 2 ; person
 
 	; warp-to
-	EVENT_DISP BEACH_HOUSE_WIDTH,7,2
-	EVENT_DISP BEACH_HOUSE_WIDTH,7,3
+	warp_to 2, 7, BEACH_HOUSE_WIDTH
+	warp_to 3, 7, BEACH_HOUSE_WIDTH
