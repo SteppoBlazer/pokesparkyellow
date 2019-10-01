d57ff89f71f4d37174e7ad31fae849ff337d5b64
SteppoBlazer <gainzgamezn@gmail.com>
2019-10-02 01:53:31 +0200

    Added few more custom mon pics and worked Polished Maps compatibility up to the Silph Co maps

--- data/mapObjects/route22.asm
+++ data/mapObjects/route22.asm
@@ -8,8 +8,8 @@ Route22Object:
 	sign 7, 11, 3 ; Route22FrontGateText
 
 	db 2 ; objects
-	object SPRITE_BLUE, 5, 25, STAY, NONE, 1 ; person
-	object SPRITE_BLUE, 5, 25, STAY, NONE, 2 ; person
+	object SPRITE_BLUE, 25, 5, STAY, NONE, 1 ; person
+	object SPRITE_BLUE, 25, 5, STAY, NONE, 2 ; person
 
 	; warp-to
 	warp_to 8, 5, ROUTE_22_WIDTH ; ROUTE_22_GATE
