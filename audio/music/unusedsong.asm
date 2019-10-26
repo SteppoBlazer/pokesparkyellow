39afd7251677c25ba1697a83999cf42db310f447
SteppoBlazer <gainzgamezn@gmail.com>
2019-10-26 10:17:11 +0200

    Perfected the unused song asm

--- audio/music/unusedsong.asm
+++ audio/music/unusedsong.asm
@@ -1,41 +1,18 @@
-Music_UnusedSong_Ch1_sub_0:
-	octave 4
-	E_ 1
-	rest 1
-	octave 3
-	B_ 1
-	rest 1
-	octave 4
-	C# 1
-	rest 1
-	octave 3
-	B_ 1
-	octave 4
-	E_ 1
-	rest 4
-	E_ 2
-	rest 2
-	D# 1
-	rest 1
-	octave 3
-	B_ 1
-	rest 1
-	octave 4
-	C# 1
-	rest 1
-	octave 3
-	B_ 1
-	octave 4
-	D# 1
-	rest 8
-	C# 1
-	rest 1
-	D# 1
-	rest 1
-	C# 1
-	octave 3
-	B_ 1
-	rest 2
+Music_UnusedSong_branch_aa6f::
+	tempo 232
+	loopchannel 0, Music_UnusedSong_derp
+
+Music_UnusedSong_Ch1:: ; a913 (2:6913)
+	tempo 144
+	
+Music_UnusedSong_derp::
+	volume 7, 7
+	duty 3
+	vibrato 5, 3, 3
+
+Music_UnusedSong_branch_a91b::
+	notetype 12, 12, 5
+	callchannel Music_UnusedSong_1s1
 	octave 4
 	C# 1
 	rest 1
@@ -56,43 +33,7 @@ Music_UnusedSong_Ch1_sub_0:
 	octave 3
 	B_ 2
 	rest 1
-	octave 4
-	E_ 1
-	rest 1
-	octave 3
-	B_ 1
-	rest 1
-	octave 4
-	C# 1
-	rest 1
-	octave 3
-	B_ 1
-	octave 4
-	E_ 1
-	rest 4
-	E_ 2
-	rest 2
-	D# 1
-	rest 1
-	octave 3
-	B_ 1
-	rest 1
-	octave 4
-	C# 1
-	rest 1
-	octave 3
-	B_ 1
-	octave 4
-	D# 1
-	rest 8
-	C# 1
-	rest 1
-	D# 1
-	rest 1
-	C# 1
-	octave 3
-	B_ 1
-	rest 2
+	callchannel Music_UnusedSong_1s1
 	octave 4
 	C# 1
 	rest 1
@@ -169,59 +110,19 @@ Music_UnusedSong_Ch1_sub_0:
 	B_ 2
 	rest 6
 	B_ 2
+	rest 6
+	loopchannel 0, Music_UnusedSong_branch_a91b
 	endchannel
-Music_UnusedSong_Ch1::
-	tempo 144
-	volume 5, 5
-	vibrato 8, 2, 4
+
+
+Music_UnusedSong_Ch2:: ; a9cf (2:69cf)
 	duty 3
+	toggleperfectpitch
+	vibrato 6, 3, 3
+
+Music_UnusedSong_branch_a9d8::
 	notetype 12, 12, 5
-	callchannel Music_UnusedSong_Ch1_sub_0
-	rest 6
-	callchannel Music_UnusedSong_Ch1_sub_0
-	rest 6
-	callchannel Music_UnusedSong_Ch1_sub_0
-	rest 6
-	callchannel Music_UnusedSong_Ch1_sub_0
-	endchannel
-Music_UnusedSong_Ch2_sub_0:
-	octave 4
-	E_ 1
-	rest 1
-	G# 1
-	rest 1
-	F# 1
-	rest 1
-	A_ 1
-	G# 1
-	rest 8
-	D# 1
-	rest 1
-	F# 1
-	rest 1
-	E_ 1
-	rest 1
-	G# 1
-	F# 1
-	rest 4
-	octave 3
-	B_ 2
-	rest 2
-	octave 4
-	C# 1
-	rest 1
-	D# 1
-	rest 1
-	E_ 1
-	F# 1
-	rest 2
-	C# 1
-	rest 1
-	D# 1
-	rest 1
-	E_ 1
-	F# 1
-	rest 2
+	callchannel Music_UnusedSong_2s1
 	F# 4
 	E_ 4
 	F# 1
@@ -231,42 +132,7 @@ Music_UnusedSong_Ch2_sub_0:
 	rest 1
 	D# 2
 	rest 1
-	E_ 1
-	rest 1
-	G# 1
-	rest 1
-	F# 1
-	rest 1
-	A_ 1
-	G# 1
-	rest 8
-	D# 1
-	rest 1
-	F# 1
-	rest 1
-	E_ 1
-	rest 1
-	G# 1
-	F# 1
-	rest 4
-	octave 3
-	B_ 2
-	rest 2
-	octave 4
-	C# 1
-	rest 1
-	D# 1
-	rest 1
-	E_ 1
-	F# 1
-	rest 2
-	C# 1
-	rest 1
-	D# 1
-	rest 1
-	E_ 1
-	F# 1
-	rest 2
+	callchannel Music_UnusedSong_2s1
 	F# 4
 	E_ 4
 	F# 1
@@ -330,53 +196,92 @@ Music_UnusedSong_Ch2_sub_0:
 	F# 2
 	rest 6
 	D# 2
+	rest 6
+	loopchannel 0, Music_UnusedSong_branch_a9d8
 	endchannel
-Music_UnusedSong_Ch2::
-	vibrato 5, 1, 5
-	duty 3
-	notetype 12, 12, 4
-	callchannel Music_UnusedSong_Ch2_sub_0
-	rest 6
-	callchannel Music_UnusedSong_Ch2_sub_0
-	rest 6
-	callchannel Music_UnusedSong_Ch2_sub_0
-	rest 6
-	callchannel Music_UnusedSong_Ch2_sub_0
-	endchannel
-Music_UnusedSong_Ch3_sub_0:
+; 0xaa6f
+
+Music_UnusedSong_1s1:
+	octave 4
+	E_ 1
+	rest 1
+	octave 3
+	B_ 1
+	rest 1
+	octave 4
+	C# 1
+	rest 1
+	octave 3
+	B_ 1
+	octave 4
+	E_ 1
+	rest 4
 	E_ 2
 	rest 2
+	D# 1
+	rest 1
+	octave 3
+	B_ 1
+	rest 1
+	octave 4
+	C# 1
+	rest 1
+	octave 3
+	B_ 1
+	octave 4
+	D# 1
+	rest 8
+	octave 4
+	C# 1
+	rest 1
+	D# 1
+	rest 1
+	C# 1
+	octave 3
+	B_ 1
+	rest 2
+	endchannel
+
+Music_UnusedSong_2s1:
+	octave 4
+	E_ 1
+	rest 1
+	G# 1
+	rest 1
+	F# 1
+	rest 1
+	A_ 1
+	G# 1
+	rest 8
+	D# 1
+	rest 1
+	F# 1
+	rest 1
+	E_ 1
+	rest 1
+	G# 1
+	F# 1
+	rest 4
 	octave 3
 	B_ 2
+	rest 2
 	octave 4
-	E_ 2
+.minisection
+	C# 1
+	rest 1
+	D# 1
+	rest 1
+	E_ 1
+	F# 1
 	rest 2
-	E_ 2
-	G# 2
-	E_ 2
-	D# 2
-	rest 2
-	octave 3
-	B_ 2
-	octave 4
-	D# 2
-	rest 2
-	D# 2
-	G# 2
-	D# 2
-	C# 2
-	rest 2
-	octave 3
-	A_ 2
-	octave 4
-	C# 2
-	rest 2
-	octave 3
-	A_ 2
-	octave 4
-	C# 2
-	octave 3
-	A_ 2
+	loopchannel 2, .minisection
+	endchannel
+
+Music_UnusedSong_Ch3:
+	notetype 12, 1, 1
+	toggleperfectpitch
+.loop
+	callchannel Music_Unused_section1
 	octave 4
 	D# 4
 	C# 2
@@ -386,39 +291,7 @@ Music_UnusedSong_Ch3_sub_0:
 	octave 4
 	C# 2
 	D# 2
-	E_ 2
-	rest 2
-	octave 3
-	B_ 2
-	octave 4
-	E_ 2
-	rest 2
-	E_ 2
-	G# 2
-	E_ 2
-	D# 2
-	rest 2
-	octave 3
-	B_ 2
-	octave 4
-	D# 2
-	rest 2
-	D# 2
-	G# 2
-	D# 2
-	C# 2
-	rest 2
-	octave 3
-	A_ 2
-	octave 4
-	C# 2
-	rest 2
-	octave 3
-	A_ 2
-	octave 4
-	C# 2
-	octave 3
-	A_ 2
+	callchannel Music_Unused_section1
 	octave 4
 	D# 4
 	C# 4
@@ -472,130 +345,119 @@ Music_UnusedSong_Ch3_sub_0:
 	F# 2
 	rest 6
 	D# 2
-	endchannel
-Music_UnusedSong_Ch3::
-	vibrato 0, 0, 0
-	notetype 12, 1, 1
-	toggleperfectpitch
+	rest 6
+	loopchannel 0, .loop
+
+Music_Unused_section1:
 	octave 4
-	callchannel Music_UnusedSong_Ch3_sub_0
-	rest 6
-	callchannel Music_UnusedSong_Ch3_sub_0
-	rest 6
-	callchannel Music_UnusedSong_Ch3_sub_0
-	rest 6
-	callchannel Music_UnusedSong_Ch3_sub_0
+	E_ 2
+	rest 2
+	octave 3
+	B_ 2
+	octave 4
+	E_ 2
+	rest 2
+	E_ 2
+	G# 2
+	E_ 2 
+	D# 2
+	rest 2
+	octave 3
+	B_ 2
+	octave 4
+	D# 2
+	rest 2
+	D# 2
+	G# 2
+	D# 2
+	C# 2
+	rest 2
+	octave 3
+	A_ 2
+	octave 4
+	C# 2
+	rest 2
+	octave 3
+	A_ 2
+	octave 4
+	C# 2
+	octave 3
+	A_ 2
 	endchannel
-Music_UnusedSong_Ch4_sub_0:
-	snare6 6
-	snare6 6
-	triangle1 6
-	triangle1 6
-	snare6 2
-	snare6 2
-	triangle2 6
-	triangle1 6
-	snare6 4
-	triangle1 6
-	triangle2 6
-	snare6 2
-	snare6 2
-	triangle1 6
-	triangle1 6
-	snare6 4
-	triangle2 6
-	triangle1 6
-	snare6 4
-	triangle1 6
-	triangle2 6
-	snare6 4
-	triangle1 6
-	triangle1 6
-	snare6 4
-	triangle2 6
-	triangle1 6
-	snare6 4
-	triangle1 6
-	triangle2 6
-	snare6 2
-	snare6 2
-	triangle1 6
-	triangle1 6
-	snare6 4
-	triangle2 6
-	triangle1 6
-	snare6 4
-	triangle1 6
-	triangle2 6
-	snare6 4
-	triangle1 6
-	triangle1 6
-	snare6 4
-	triangle2 6
-	triangle1 6
-	snare6 4
-	triangle1 6
-	triangle2 6
-	snare6 2
-	snare6 2
-	triangle1 6
-	triangle1 6
-	snare6 4
-	triangle2 6
-	triangle1 6
-	snare6 2
-	snare6 2
-	triangle1 6
-	triangle2 6
-	snare6 4
-	triangle1 6
-	triangle1 6
-	snare6 4
-	triangle2 6
-	triangle1 6
-	snare6 4
-	triangle1 6
-	triangle2 6
-	snare6 2
-	snare6 2
-	triangle1 6
-	triangle1 6
-	snare6 4
-	triangle2 6
-	triangle1 6
-	snare6 2
-	snare6 2
-	triangle1 6
-	triangle2 6
-	snare6 4
-	triangle1 6
-	triangle1 6
-	snare6 4
-	triangle2 6
-	triangle1 6
-	snare6 2
-	snare6 2
-	triangle1 6
-	triangle2 6
-	snare6 4
-	triangle1 6
-	triangle1 6
-	snare6 4
-	triangle2 6
-	triangle1 6
-	snare6 2
-	snare6 2
-	triangle1 6
-	triangle2 6
-	snare6 4
-	triangle1 6
-	triangle1 6
-	snare6 4
-	endchannel
-Music_UnusedSong_Ch4::
+	
+Music_UnusedSong_Ch4:: ; acef (2:6cef)
 	dspeed 12
+	callchannel Music_UnusedSong_branch_ad36
+
+Music_UnusedSong_branch_acf3::
+	callchannel Music_UnusedSong_branch_ad36
+	callchannel Music_UnusedSong_branch_ad45
+	callchannel Music_UnusedSong_branch_ad45
+	loopchannel 2, Music_UnusedSong_branch_acf3
+	callchannel Music_UnusedSong_branch_ad36
+	callchannel Music_UnusedSong_branch_ad5f
+	callchannel Music_UnusedSong_branch_ad52
+	triangle1 6
+	triangle1 6
+	triangle2 4
+	callchannel Music_UnusedSong_branch_ad6e
+	callchannel Music_UnusedSong_branch_ad5f
+	callchannel Music_UnusedSong_branch_ad52
+	callchannel Music_UnusedSong_branch_ad6e
+	callchannel Music_UnusedSong_branch_ad52
+	triangle1 6
+	triangle1 6
+	triangle2 2
+	triangle1 2
+	triangle1 6
+	triangle1 6
+	triangle1 4
+	triangle1 6
 	snare6 6
-	callchannel Music_UnusedSong_Ch4_sub_0
+	snare6 4
+	loopchannel 0, Music_UnusedSong_Ch4
+
+Music_UnusedSong_branch_ad36::
 	snare6 6
-	callchannel Music_UnusedSong_Ch4_sub_0
+	snare6 6
+	snare6 4
+	snare6 6
+	snare6 6
+	snare6 2
+	snare6 2
+	endchannel
+
+Music_UnusedSong_branch_ad45::
+	snare6 6
+	snare6 6
+	snare6 4
+	snare6 6
+	snare6 6
+	snare6 4
+	endchannel
+
+Music_UnusedSong_branch_ad52::
+	triangle1 6
+	triangle1 6
+	triangle2 4
+	triangle1 6
+	triangle1 6
+	triangle2 4
+	endchannel
+
+Music_UnusedSong_branch_ad5f::
+	triangle1 6
+	triangle1 6
+	triangle2 4
+	triangle1 6
+	triangle1 6
+	triangle2 2
+	triangle1 2
+	endchannel
+
+Music_UnusedSong_branch_ad6e::
+	triangle1 6
+	triangle1 6
+	triangle2 2
+	triangle2 2
 	endchannel
