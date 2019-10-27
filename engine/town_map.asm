2477f60ce4386a4b572ce191696cfcfa93fa9275
SteppoBlazer <gainzgamezn@gmail.com>
2019-10-27 08:21:17 +0100

    Cerulean Cave mons show up if nested there, fixed issue of garbage tiles on screen after exiting town map screen

--- engine/town_map.asm
+++ engine/town_map.asm
@@ -343,6 +343,8 @@ ExitTownMap:
 	xor a
 	ld [wTownMapSpriteBlinkingEnabled], a
 	call GBPalWhiteOut
+	call ReloadMapData
+	call RestoreScreenTilesAndReloadTilePatterns
 	call ClearScreen
 	call ClearSprites
 	call LoadPlayerSpriteGraphics
@@ -391,8 +393,6 @@ DisplayWildLocations:
 	call LoadTownMapEntry
 	pop hl
 	ld a, [de]
-	cp $19 ; Cerulean Cave's coordinates
-	jr z, .nextEntry ; skip Cerulean Cave
 	call TownMapCoordsToOAMCoords
 	ld a, $4 ; nest icon tile no.
 	ld [hli], a
